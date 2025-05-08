54003bab720e2cfea2c11e730b6d84b61a30d452
XspsSlayer <160418935+XspsSlayer@users.noreply.github.com>
2025-05-08 16:23:21 +0300

    Create variables.php

---
+++ PHP/variables.php
@@ -0,0 +1,13 @@
+<?php
+
+$akeraios_x = 5;
+$akeraios_y = 10;
+$float = 1.3;
+$bt = true;
+$your_name = "Σπύρος";
+echo "Πρόσθεση δύο μεταβλητών $akeraios_x + $akeraios_y = ",$akeraios_x + $akeraios_y, "<br>";
+echo "Ο τυπος δεδομενων ειναι ",var_dump($akeraios_x), "<br>";
+echo "Το όνομα μου ειναι $your_name και ο τύπος δεδομένων μου ειναι ",var_dump($your_name), "<br>";
+echo "Ο τυπος δεδομενων ειναι ",var_dump($float), "<br>";
+echo "Ο τυπος δεδομενων ειναι ",var_dump($bt);
+?>
