904cc437265cede7d5f6bcfa722606ffd1bd294d
SpyrosVenetis <160418935+SpyrosVenetis@users.noreply.github.com>
2025-10-13 18:02:39 +0300

    Add files via upload

---
+++ Java/Fharenheit To Celsius.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.println("Dwse tous vathmous C");
+		Scanner input = new Scanner(System.in);
+		Double C = input.nextDouble();
+		double F = (C * 9/5) + 32;
+		System.out.println("oi "+ C +" vathmoi kelsiou einai "+ F +" vathmoi Fahrenheit");
+	}
+}
