8ecc26498cba19013fc4ff38888c1fead3d3cae6
SpyrosVenetis <160418935+SpyrosVenetis@users.noreply.github.com>
2025-10-20 16:27:29 +0300

    Update MiniCalc.java

--- Java/MiniCalc.java
+++ Java/MiniCalc.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 public class Main
 {
-	public static void main(String[] args) {
+	public static void MiniCalc(String[] args) {
 		Scanner input = new Scanner(System.in);
 		double num1 = input.nextDouble();
 		input.nextLine();
